0841f43619f7002764ad971d98fe431ddc42fece
Sean Rinkleff <seanrinkleff@gmail.com>
2023-12-06 18:22:53 -0500

    controller skeletons

---
+++ src/main/java/controllers/ProjectListController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class ProjectListController {
+
+    @FXML
+    private Button account;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    void primary(MouseEvent event) {
+
+    }
+
+}
