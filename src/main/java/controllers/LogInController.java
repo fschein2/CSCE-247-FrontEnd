647ea7797ad03ca76cd40d212e436a724bd325c1
fschein <fschein02@gmail.com>
2023-12-06 18:30:15 -0500

    fixed

--- src/main/java/controllers/LogInController.java
+++ src/main/java/controllers/LogInController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class LogInController {
+public class LoginController {
 
     @FXML
     private Button loginButton;
